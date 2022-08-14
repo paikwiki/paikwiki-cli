647f1826565e2e3257e86748c2e92ec015fc5d0f
paikwiki <paikwiki@gmail.com>
2022-08-14 14:04:39 +0900

    :sparkles: Add "--do" option on "run" command

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,30 @@ const greetingOption: CommandOption = {
   defaultValue: "Hi",
 };
 
+const doFunctionOption: CommandOption = {
+  flag: "--do <doFunctionName>",
+  description: 'available functions: "addQuestionMark", "addTilde"',
+  defaultValue: "default",
+};
+
+const doFunctions: { [functionName: string]: (param: string) => void } = {
+  default: (greeting: string) => console.log(greeting),
+  addQuestionMark: (greeting: string) => console.log(`${greeting}?`),
+  addTilde: (greeting: string) => console.log(`${greeting}~`),
+};
+
+const commandAction = (optionStrings: { [k: string]: string }) => {
+  let doFunction = doFunctions["default"];
+  if (optionStrings["do"]) doFunction = doFunctions[optionStrings["do"]];
+
+  try {
+    doFunction(optionStrings.greeting);
+  } catch (error) {
+    console.log(`--do "${optionStrings["do"]}" is invalid`);
+    process.exit(1);
+  }
+};
+
 const main = (() => {
   program
     .name("Paikwiki CLI")
@@ -18,10 +42,8 @@ const main = (() => {
     .command("run")
     .description('run "run" command(FOR TEST)')
     .option(...commandOptionConverter(greetingOption))
-    .action((str) => {
-      const keys = Object.keys(str);
-      keys.forEach((key) => console.log(key, str[key]));
-    });
+    .option(...commandOptionConverter(doFunctionOption))
+    .action(commandAction);
 
   program.parse();
 })();
