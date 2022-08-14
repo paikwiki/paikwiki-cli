054925db79604a2a514a3f4b92bf1ad6c6bd788d
paikwiki <paikwiki@gmail.com>
2022-08-15 03:01:57 +0900

    :recycle: Refactor greetingCommand

--- src/commands/greetingCommand.ts
+++ src/commands/greetingCommand.ts
@@ -1,20 +1,40 @@
 import type { ProgramCommand } from "../types";
 
+type GreetingCommandOption = "--message" | "--addSuffix";
+type GreetingCommandOptionName = "message" | "addSuffix";
 type AddSuffixFunctionName = "default" | "questionMark" | "tilde";
 type AddSuffix = (param: AddSuffixFunctionName) => string;
 
-const ADD_SUFFIX_FUNCTION_NAMES: Readonly<
-  Record<AddSuffixFunctionName, AddSuffixFunctionName>
-> = {
-  default: "default",
-  questionMark: "questionMark",
-  tilde: "tilde",
+const GREETING_COMMAND_NAME = "greeting";
+
+const nameObjectConverter = <T extends string>(
+  names: T[]
+): Readonly<Record<T, T>> => {
+  const nameObject: Partial<Record<T, T>> = {};
+  names.forEach((name) => (nameObject[name] = name));
+
+  return nameObject as Readonly<Record<T, T>>; // TODO: as 제거
 };
+
+const greetingCommandOptions: Readonly<
+  Record<GreetingCommandOptionName, GreetingCommandOption>
+> = {
+  message: "--message",
+  addSuffix: "--addSuffix",
+};
+
+const addSuffixFunctionNames: Readonly<
+  Record<AddSuffixFunctionName, AddSuffixFunctionName>
+> = nameObjectConverter(["default", "questionMark", "tilde"]);
 
 const getAddSuffixDescription = (functionNames: string[]) =>
   `available functions: ${functionNames.map((name) => `"${name}"`).join(", ")}`;
 
 const action = (optionStrings: { [k: string]: string }) => {
+  const greetingCommandOptionNames: Readonly<
+    Record<GreetingCommandOptionName, GreetingCommandOptionName>
+  > = nameObjectConverter(["message", "addSuffix"]);
+
   const greetWithSuffix = (greeting: string) => (suffix?: string) =>
     suffix ? `${greeting}${suffix}` : greeting;
 
@@ -28,33 +48,35 @@ const action = (optionStrings: { [k: string]: string }) => {
 
   const addSuffix =
     addSuffixes[
-      (optionStrings["addSuffix"] ??
-        ADD_SUFFIX_FUNCTION_NAMES.default) as AddSuffixFunctionName // TODO: as 제거
+      (optionStrings[greetingCommandOptionNames.addSuffix] ??
+        addSuffixFunctionNames.default) as AddSuffixFunctionName // TODO: as 제거
     ];
 
   try {
     console.log(addSuffix(optionStrings.message as AddSuffixFunctionName)); // TODO: as 제거
   } catch (error) {
-    console.log(`--addSuffix "${optionStrings["addSuffix"]}" is invalid`);
+    console.log(
+      `${greetingCommandOptions.addSuffix} "${
+        optionStrings[greetingCommandOptionNames.addSuffix]
+      }" is invalid`
+    );
     process.exit(1);
   }
 };
 
 export const greetingCommand: ProgramCommand = {
-  commandName: "greeting",
+  commandName: GREETING_COMMAND_NAME,
   description: "print greeting message(for test)",
   options: [
     {
-      flag: "--message <greetingMessage>",
+      flag: `${greetingCommandOptions.message} <greetingMessage>`,
       description: "add custom greeting message",
       defaultValue: "Hi",
     },
     {
-      flag: "--addSuffix <addSuffixFunctionName>",
-      description: getAddSuffixDescription(
-        Object.keys(ADD_SUFFIX_FUNCTION_NAMES)
-      ),
-      defaultValue: ADD_SUFFIX_FUNCTION_NAMES.default,
+      flag: `${greetingCommandOptions.addSuffix} <addSuffixFunctionName>`,
+      description: getAddSuffixDescription(Object.keys(addSuffixFunctionNames)),
+      defaultValue: addSuffixFunctionNames.default,
     },
   ],
   action,
