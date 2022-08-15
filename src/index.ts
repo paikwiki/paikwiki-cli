af4b20a18be4243d1c6b17ed84fed24513b2b2eb
paikwiki <paikwiki@gmail.com>
2022-08-15 19:18:25 +0900

    :technologist: Improve method to add command(add addCommands)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { program } from "commander";
-import { addCommand, getProgram } from "./program";
+import { addCommands, getProgram } from "./program";
 import { programInfo } from "./programInfo";
 import { greetingCommand } from "./commands/greetingCommand";
 import { catCommand } from "./commands/catCommand";
 
 const programInstance = getProgram(program, programInfo);
-addCommand(programInstance, greetingCommand);
-addCommand(programInstance, catCommand);
+const commands = [greetingCommand, catCommand];
+
+addCommands(programInstance, commands);
 
 programInstance.parse();
--- src/program.ts
+++ src/program.ts
@@ -2,12 +2,7 @@ import { commandOptionConverter } from "./utils";
 import type { Command } from "commander";
 import type { ProgramCommand, ProgramInfo } from "./types";
 
-export const getProgram = (
-  program: Command,
-  { name, description, version }: ProgramInfo
-) => program.name(name).description(description).version(version);
-
-export const addCommand = (
+const addCommand = (
   programInstance: Command,
   { commandName, description, options, action }: ProgramCommand
 ) => {
@@ -17,6 +12,17 @@ export const addCommand = (
     pi.option(...commandOptionConverter(opt));
   });
   pi.action(action);
-
-  return pi;
 };
+
+export const addCommands = (
+  programInstance: Command,
+  programCommands: ProgramCommand[]
+) =>
+  programCommands.forEach((programCommand) =>
+    addCommand(programInstance, programCommand)
+  );
+
+export const getProgram = (
+  program: Command,
+  { name, description, version }: ProgramInfo
+) => program.name(name).description(description).version(version);
