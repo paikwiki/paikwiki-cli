ad662923e2b39057723ed8a2e3be5b1fbdf5521f
paikwiki <paikwiki@gmail.com>
2022-08-15 13:36:32 +0900

    :recycle: Refactor greetingCommand

--- src/commands/greetingCommand.ts
+++ src/commands/greetingCommand.ts
@@ -5,47 +5,62 @@ type GreetingCommandOptionName = "message" | "addSuffix";
 type AddSuffixFunctionName = "default" | "questionMark" | "tilde";
 type AddSuffix = (param: AddSuffixFunctionName) => string;
 
+const OPTION_PREFIX = "--";
 const GREETING_COMMAND_NAME = "greeting";
+const GREETING_COMMAND_OPTION_NAMES: Readonly<GreetingCommandOptionName[]> = [
+  "message",
+  "addSuffix",
+] as const;
+const ADD_SUFFIX_FUNCTION_NAMES: Readonly<AddSuffixFunctionName[]> = [
+  "default",
+  "questionMark",
+  "tilde",
+] as const;
 
-const nameObjectConverter = <T extends string>(
-  names: T[]
-): Readonly<Record<T, T>> => {
-  const nameObject: Partial<Record<T, T>> = {};
-  names.forEach((name) => (nameObject[name] = name));
+const nameObjectConverter = <T extends string, U extends string>(
+  names: Readonly<T[]>,
+  withPrefixForValue?: string
+): Readonly<Record<T, U>> => {
+  const nameObject: Partial<Record<T, U>> = {};
+  names.forEach((name) => {
+    nameObject[name] = (
+      withPrefixForValue ? `${withPrefixForValue}${name}` : name
+    ) as U;
+  });
 
-  return nameObject as Readonly<Record<T, T>>; // TODO: as 제거
+  return nameObject as Readonly<Record<T, U>>; // TODO: as 제거
 };
 
 const greetingCommandOptions: Readonly<
   Record<GreetingCommandOptionName, GreetingCommandOption>
-> = {
-  message: "--message",
-  addSuffix: "--addSuffix",
-};
+> = nameObjectConverter(GREETING_COMMAND_OPTION_NAMES, OPTION_PREFIX);
 
 const addSuffixFunctionNames: Readonly<
   Record<AddSuffixFunctionName, AddSuffixFunctionName>
-> = nameObjectConverter(["default", "questionMark", "tilde"]);
+> = nameObjectConverter(ADD_SUFFIX_FUNCTION_NAMES);
 
 const getAddSuffixDescription = (functionNames: string[]) =>
   `available functions: ${functionNames.map((name) => `"${name}"`).join(", ")}`;
 
 const action = (optionStrings: { [k: string]: string }) => {
+  const suffixes = {
+    questionMark: "!",
+    tilde: "~",
+  };
   const greetingCommandOptionNames: Readonly<
     Record<GreetingCommandOptionName, GreetingCommandOptionName>
-  > = nameObjectConverter(["message", "addSuffix"]);
-
-  const greetWithSuffix = (greeting: string) => (suffix?: string) =>
-    suffix ? `${greeting}${suffix}` : greeting;
-
+  > = nameObjectConverter(GREETING_COMMAND_OPTION_NAMES);
   const addSuffixes: Readonly<{
     [functionName in AddSuffixFunctionName]: AddSuffix;
   }> = {
     default: (greeting) => greetWithSuffix(greeting)(),
-    questionMark: (greeting) => greetWithSuffix(greeting)("?"),
-    tilde: (greeting) => greetWithSuffix(greeting)("~"),
+    questionMark: (greeting) =>
+      greetWithSuffix(greeting)(suffixes.questionMark),
+    tilde: (greeting) => greetWithSuffix(greeting)(suffixes.tilde),
   };
 
+  const greetWithSuffix = (greeting: string) => (suffix?: string) =>
+    suffix ? `${greeting}${suffix}` : greeting;
   const addSuffix =
     addSuffixes[
       (optionStrings[greetingCommandOptionNames.addSuffix] ??
