17f0b3d6930a609b3c06899e150c486d85dad794
paikwiki <paikwiki@gmail.com>
2023-10-23 08:56:50 +0900

    :wrench: 버전 정보 설정에서 가져오도록 변경

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import App from "./app";
 import { greetingCommand } from "./commands/greetingCommand";
 import { catCommand } from "./commands/catCommand";
 
+const version = process.env.npm_package_version;
+if (!version) throw new Error(`version is not set on package.json`);
+
 const app = new App(
   {
     name: "Paikwiki CLI",
     description: "CLI Tool created by paikwiki",
-    version: "1.0.3",
+    version,
   },
   [greetingCommand, catCommand],
 );
